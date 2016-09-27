3cba2a55c2c98c936e2f6d3b34bf698c7a06037d
Derrick Kwagala <derrick.kwagala@gmail.com>
2016-09-27 21:21:29 +0300

    Added typings

---
+++ src/TimeSeriesChart/widget/components/Wrapper.js
@@ -0,0 +1,51 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+define(["require", "exports", "TimeSeriesChart/lib/react", "TimeSeriesChart/lib/d3", "TimeSeriesChart/lib/react-nvd3", "TimeSeriesChart/lib/nv.d3"], function (require, exports, React, d3, NVD3Chart) {
+    "use strict";
+    var Wrapper = (function (_super) {
+        __extends(Wrapper, _super);
+        function Wrapper(props) {
+            _super.call(this, props);
+            logger.debug(this.props.widgetId + ".constructor");
+            this.state = {
+                visible: this.props.visible,
+            };
+            this.changeVisibility = this.changeVisibility.bind(this);
+        }
+        Wrapper.prototype.changeVisibility = function () {
+            logger.debug(this.props.widgetId + ".changeVisibility");
+            this.setState({ visible: !this.state.visible });
+        };
+        Wrapper.prototype.render = function () {
+            logger.debug(this.props.widgetId + ".render");
+            var chart;
+            var context = {
+                getColor: function (iparam) {
+                    var colors = d3.scale.category20().range().slice(10);
+                    return colors[Math.floor(Math.random() * colors.length)];
+                },
+            };
+            if (this.state.visible) {
+                logger.debug(this.props.widgetId + ".render.visibleTrue");
+                logger.debug(nv);
+                return (React.createElement("div", null, 
+                    React.createElement("button", {onClick: this.changeVisibility}, " Click me"), 
+                    React.createElement(NVD3Chart, {context: context, color: { name: 'getColor', type: 'function' }, tooltip: { enabled: true }, type: "discreteBarChart", datum: this.props.datum, x: "label", y: "value"})));
+            }
+            return (React.createElement("div", null, 
+                React.createElement("button", {onClick: this.changeVisibility}, " Click me")
+            ));
+        };
+        Wrapper.defaultProps = {
+            widgetId: "",
+            datum: [],
+            visible: true
+        };
+        return Wrapper;
+    }(React.Component));
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = Wrapper;
+});
---
+++ typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="modules/d3/index.d.ts" />
+/// <reference path="modules/nvd3/index.d.ts" />
