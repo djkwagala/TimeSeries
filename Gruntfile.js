aef79598c1de6b1b2ec50b8fa7be0b46f3811872
Derrick Kwagala <derrick.kwagala@gmail.com>
2016-10-04 19:09:53 +0300

    Add xsltproc

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,13 +69,24 @@ module.exports = function (grunt) {
                     "./test/widgets/" + pkg.name + ".mpk"
                 ],
             out : "./out/**/*"                
-        }
+        },
+        xsltproc: {
+            options: {
+                stylesheet: 'widget.xsl'
+            },
+            compile: {
+                files: {
+                    "src/<%=pkgName%>/<%=name%>.d.ts" : ["src/" + pkg.name + "/" + pkg.name + ".xml"]
+                }
+            }
+        } 
     });
     
     grunt.loadNpmTasks("grunt-contrib-compress");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-xsltproc");
     grunt.registerTask("default", ["watch"]);
     grunt.registerTask("distribute", ["clean:out", "copy:out", "compress:out", "copy:mpks" ]);
     grunt.registerTask(
